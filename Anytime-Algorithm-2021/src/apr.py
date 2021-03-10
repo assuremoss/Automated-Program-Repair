c7f7c6fb126db9e5672c2b8cf4798f9ce7a7410e
vuduclyunitn <ducly.vu@unitn.it>
2021-03-10 04:54:43 -0500

    Refactor the APR class

--- Anytime-Algorithm-2021/src/apr.py
+++ Anytime-Algorithm-2021/src/apr.py
@@ -6,7 +6,60 @@ import subprocess
 import shutil
 import os
 import math
+from dataclasses import dataclass
 import pandas as pd
+import tempfile
+
+
+@dataclass
+class APR:
+    project_id: str
+    bug_id: str
+    tool: str
+
+    def worker(self, time_budget: int, patches_dir: str = None) -> str:
+        """TODO: Docstring for running.
+        Running the tool on the bug within a specific time budget
+        :time_budget: Time to repair a bug in minutes 
+        :returns: Path to the result file containg plausible patch
+        """
+        result_path = "/tmp/results:/results"
+        if os.path.exists(result_path):
+            shutil.rmtree(result_path)
+        command = ["docker", "run", "-it", "--rm",
+            "-v", result_path, "tdurieux/astor",
+            "-i", self.bug_id, 
+            "--parameters", 
+            "maxtime:" + str(time_budget) + 
+            ":" + "stopfirst:true" + ":saveall:false"+
+            ":mode:" + str(repair_tool),
+        ]
+
+        subprocess.run(command)
+        return result_path
+
+    def repair(self):
+        """
+        :project_id: Project ID
+        :repair_too: automated software repair tools jgenprog, jkali,...
+        :timeout: User-defined timeout
+        :returns: TODO
+
+        """
+        data_frame = pd.read_excel(excel_file)
+        for _, row in data_frame.iterrows():
+            if row["PID"] == project_id:
+                timeout = math.ceil(trial * ((row["CT"] + row["TT"]) / 60))
+                bug_id = str(row["PID"]) + "-" + str(row["BID"])
+                result_path = running(bug_id, repair_tool, timeout)
+        result_path = os.path.join(
+            os.getenv("DATA_INTERIM_DIR"), project_id + "_" + repair_tool, str(trial) + "T"
+        )
+        if os.path.exists(result_path):
+            shutil.rmtree(result_path)
+        src_dir = os.path.join(os.sep, "tmp", "results", "Defects4J")
+        shutil.copytree(src_dir, result_path)
+        shutil.rmtree(src_dir)
 
 def argument_processing():
     """TODO: Docstring for argment_processing.
@@ -24,62 +77,8 @@ def argument_processing():
     return args
 
 
-def running(bug_id, repair_tool, timeout):
-    """TODO: Docstring for running.
-    Running the tool on the bug within a specific timeout
-    :bug_id: Bug ID follow the pattern: ProjectID_BugID
-    :returns: TODO
-    """
-    result_path = "/tmp/results:/results"
-    if os.path.exists(result_path):
-        shutil.rmtree(result_path)
-    command = [
-        "docker",
-        "run",
-        "-it",
-        "--rm",
-        "-v",
-        result_path,
-        "tdurieux/astor",
-        "-i",
-        bug_id,
-        "--parameters",
-        "maxtime:"
-        + str(timeout)
-        + ":"
-        + "stopfirst:true"
-        + ":saveall:false"
-        + ":mode:"
-        + str(repair_tool),
-    ]
-
-    subprocess.run(command)
-    return result_path
 
 
-def running_with_timeout(excel_file, project_id, repair_tool, trial):
-    """TODO: Docstring for running_with_timeout.
-    :excel_file: The excel file containing bug data
-    :project_id: Project ID
-    :repair_too: automated software repair tools jgenprog, jkali,...
-    :timeout: User-defined timeout
-    :returns: TODO
-
-    """
-    data_frame = pd.read_excel(excel_file)
-    for _, row in data_frame.iterrows():
-        if row["PID"] == project_id:
-            timeout = math.ceil(trial * ((row["CT"] + row["TT"]) / 60))
-            bug_id = str(row["PID"]) + "-" + str(row["BID"])
-            result_path = running(bug_id, repair_tool, timeout)
-    result_path = os.path.join(
-        os.getenv("DATA_INTERIM_DIR"), project_id + "_" + repair_tool, str(trial) + "T"
-    )
-    if os.path.exists(result_path):
-        shutil.rmtree(result_path)
-    src_dir = os.path.join(os.sep, "tmp", "results", "Defects4J")
-    shutil.copytree(src_dir, result_path)
-    shutil.rmtree(src_dir)
 
 def main():
     """TODO: Docstring for main.
